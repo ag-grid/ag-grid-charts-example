0f87c3df965b7f043e1b16899bdab9039e5bb1dc
Vitaly Kravchenko <vitalyx@gmail.com>
2019-01-04 19:47:03 +0300

    Scene graph test.

---
+++ src/scene-test/index.ts
@@ -0,0 +1,57 @@
+import {Scene} from "ag-grid-enterprise/src/charts/scene/scene";
+import {Group} from "ag-grid-enterprise/src/charts/scene/group";
+import {Rect} from "ag-grid-enterprise/src/charts/scene/rect";
+import {FpsCounter} from "ag-grid-enterprise/src/charts/scene/fpsCounter";
+
+document.addEventListener('DOMContentLoaded', () => {
+    const scene = new Scene(document.body, 800, 400);
+    const group = new Group();
+
+    const n = 1000;
+    const width = scene.width;
+    const height = scene.height;
+
+    const rects: Rect[] = [];
+    const deltas: [number, number][] = [];
+    for (let i = 0; i < n; i++) {
+        const arc = new Rect();
+        // Same performance (frame rate) using instances of the same Arc.
+        // const arc = Object.create(arcTpl) as Arc;
+        arc.x = Math.random() * width;
+        arc.y = Math.random() * height;
+        rects.push(arc);
+
+        deltas.push([Math.random() - 0.5, Math.random() - 0.5]);
+    }
+    group.add(...rects);
+
+    scene.root = group;
+
+    const fpsCounter = new FpsCounter(document.body);
+
+    (function step() {
+        fpsCounter.countFrame();
+        rects.forEach((rect, i) => {
+            const delta = deltas[i];
+            rect.x += delta[0];
+            rect.y += delta[1];
+            if (rect.x > width) {
+                rect.x -= width;
+            }
+            else if (rect.x < 0) {
+                rect.x += width;
+            }
+            if (rect.y > height) {
+                rect.y -= height;
+            }
+            else if (rect.y < 0) {
+                rect.y += height;
+            }
+        });
+        // Manual rendering, without automatically setting the `dirty`
+        // flag on property changes, which triggers a re-render, also
+        // doesn't have any positive effect on frame rates.
+        // scene.render();
+        requestAnimationFrame(step);
+    })();
+});
