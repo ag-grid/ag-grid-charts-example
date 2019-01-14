ed4a46bb1e1894da5e7edc013264baa5df4966cf
Vitaly Kravchenko <vitalyx@gmail.com>
2019-01-14 17:47:57 +0300

    Test stacked transformations (group + node).

--- src/scene-test-stacked-transform/index.ts
+++ src/scene-test-stacked-transform/index.ts
@@ -13,30 +13,27 @@ document.addEventListener('DOMContentLoaded', () => {
     const group = new Group();
 
     const arc = new Arc();
-    arc.centerX = 100;
+    arc.centerX = 50;
     arc.centerY = 100;
     arc.radiusX = 30;
     arc.radiusY = 20;
 
-    arc.scalingX = 1.5;
-    arc.scalingY = 2;
-    arc.rotation = Math.PI / 4;
-    arc.translationX = 100;
-    arc.translationY = -100;
-
     const rect = new Rect();
-    rect.x = 200;
+    rect.x = 100;
     rect.y = 100;
     rect.width = 50;
     rect.height = 50;
 
-    rect.scalingX = 1.5;
-    rect.scalingY = 2;
-    rect.rotation = Math.PI / 4;
-    rect.translationX = 100;
-    rect.translationY = -100;
+    rect.scalingX = 2;
+    rect.rotationDeg = 10;
+    rect.translationY = -50;
 
     group.add([arc, rect]);
+    // The order of transformation doesn't matter here.
+    // It's always: scale, rotate, translate.
+    group.scalingY = 1.5;
+    group.rotationDeg = -10;
+    group.translationX = 50;
 
     scene.root = group;
 
@@ -48,21 +45,23 @@ document.addEventListener('DOMContentLoaded', () => {
         .attr('height', 400)
         .append('g');
 
+    // The order of transformations matters here.
+    g.attr('transform', 'translate(50, 0) rotate(-10) scale(1, 1.5)');
+
     g.append('ellipse')
-        .attr('cx', 100)
+        .attr('cx', 50)
         .attr('cy', 100)
         .attr('rx', 30)
         .attr('ry', 20)
         .attr('fill', 'red')
-        .attr('stroke', 'black')
-        .attr('transform', 'translate(100, -100) rotate(45) scale(1.5, 2)');
+        .attr('stroke', 'black');
 
     g.append('rect')
-        .attr('x', 200)
+        .attr('x', 100)
         .attr('y', 100)
         .attr('width', 50)
         .attr('height', 50)
         .attr('fill', 'red')
         .attr('stroke', 'black')
-        .attr('transform', 'translate(100, -100) rotate(45) scale(1.5, 2)');
+        .attr('transform', 'translate(0, -50) rotate(10) scale(2, 1)');
 });
