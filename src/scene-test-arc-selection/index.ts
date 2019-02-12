0cdf6a5fbf33fbddba4cb0b27fb9e2e8b041a606
Vitaly Kravchenko <vitalyx@gmail.com>
2019-02-12 13:05:08 +0300

    Selection/scene graph performance test.

--- src/scene-test-arc-selection/index.ts
+++ src/scene-test-arc-selection/index.ts
@@ -1,13 +1,8 @@
 import {Scene} from "ag-grid-enterprise/src/charts/scene/scene";
 import {Group} from "ag-grid-enterprise/src/charts/scene/group";
 import {Arc} from "ag-grid-enterprise/src/charts/scene/shape/arc";
+import {Selection} from "ag-grid-enterprise/src/charts/scene/selection";
 import {FpsCounter} from "ag-grid-enterprise/src/charts/scene/fpsCounter";
-
-// This example uses translationX/Y properties to position arcs,
-// so that the arc paths are not recalculated on every frame.
-// Instead, the transformation matrix of each arc is recalculated
-// on every frame. This should result in a better performance,
-// especially in case of complex paths.
 
 document.addEventListener('DOMContentLoaded', () => {
     const width = 800;
@@ -16,13 +11,16 @@ document.addEventListener('DOMContentLoaded', () => {
     scene.parent = document.body;
     const group = new Group();
 
-    const n = 1000;
-
-    const arcs: Arc[] = [];
     const deltas: [number, number][] = [];
 
+    const n = 1000;
     for (let i = 0; i < n; i++) {
-        const arc = new Arc();
+        deltas.push([Math.random() - 0.5, Math.random() - 0.5]);
+    }
+
+    const selection = Selection.select(group).selectAll().setData(deltas).enter.append(Arc);
+
+    selection.each(arc => {
         arc.centerX = 0;
         arc.centerY = 0;
         arc.radiusX = 7;
@@ -33,11 +31,7 @@ document.addEventListener('DOMContentLoaded', () => {
         arc.fillStyle = 'red';
         arc.strokeStyle = 'black';
         arc.lineWidth = 3;
-        arcs.push(arc);
-
-        deltas.push([Math.random() - 0.5, Math.random() - 0.5]);
-    }
-    group.append(arcs);
+    });
 
     scene.root = group;
 
@@ -45,11 +39,10 @@ document.addEventListener('DOMContentLoaded', () => {
 
     (function step() {
         fpsCounter.countFrame();
-        arcs.forEach((arc, i) => {
-            const delta = deltas[i];
 
-            arc.translationX += delta[0];
-            arc.translationY += delta[1];
+        selection.each((arc, datum) => {
+            arc.translationX += datum[0];
+            arc.translationY += datum[1];
 
             if (arc.translationX > width) {
                 arc.translationX -= width;
