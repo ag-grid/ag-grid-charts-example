78a7f91dec2d4baf80ac915089ed128ae14c6980
Vitaly Kravchenko <vitalyx@gmail.com>
2019-02-12 12:52:06 +0300

    ClipRect example/tests.

---
+++ src/clip-rect-test/index.ts
@@ -0,0 +1,98 @@
+import {Scene} from "ag-grid-enterprise/src/charts/scene/scene";
+import {Group} from "ag-grid-enterprise/src/charts/scene/group";
+import {Text} from "ag-grid-enterprise/src/charts/scene/shape/text";
+import {Arc} from "ag-grid-enterprise/src/charts/scene/shape/arc";
+import {Line} from "ag-grid-enterprise/src/charts/scene/shape/line";
+import {ClipRect} from "ag-grid-enterprise/src/charts/scene/clipRect";
+import {Selection} from "ag-grid-enterprise/src/charts/scene/selection";
+
+function delay() {
+    return new Promise(resolve => {
+        setTimeout(resolve, 2000);
+    });
+}
+
+document.addEventListener('DOMContentLoaded', () => {
+    testClipRect();
+    testClipRectSelection();
+});
+
+function testClipRect() {
+    const scene = new Scene(500, 500);
+    scene.parent = document.body;
+    const rootGroup = new Group();
+
+    const clipRect = new ClipRect();
+    clipRect.x = 50;
+    clipRect.y = 50;
+    clipRect.width = 200;
+    clipRect.height = 200;
+
+    const label = new Text();
+    label.text = 'Spectacular';
+    label.font = '20px Verdana';
+    label.textAlign = 'right';
+    label.x = 140;
+    label.y = 200;
+
+    const arc1 = Arc.create(50, 50, 50, 50, 0, Math.PI * 2);
+    arc1.fillStyle = 'red';
+
+    const arc2 = Arc.create(200, 100, 100, 100, 0, Math.PI * 2);
+    arc2.fillStyle = 'lime';
+    arc2.opacity = 0.7;
+
+    const hLine = Line.create(0, 150, 300, 150);
+    const vLine = Line.create(150, 0, 150, 300);
+
+    clipRect.append([arc1, label, hLine, vLine]);
+    rootGroup.append([clipRect, arc2]);
+
+    scene.root = rootGroup;
+
+    delay().then(() => {
+        clipRect.x += 10;
+        clipRect.y += 10;
+    }).then(delay).then(() => {
+        rootGroup.translationX = 100;
+        rootGroup.translationY = 100;
+    });
+}
+
+function testClipRectSelection() {
+    const scene = new Scene(500, 500);
+    scene.parent = document.body;
+    const rootGroup = new Group();
+    scene.root = rootGroup;
+
+    const clipSelection = Selection.select(rootGroup).append(ClipRect)
+        .each(clip => {
+            clip.x = 40;
+            clip.y = 50;
+            clip.width = 200;
+            clip.height = 200;
+        });
+
+    const groupSelection = clipSelection.append(Group);
+
+    clipSelection.append(Text)
+        .each(label => {
+            label.text = 'Spectacular';
+            label.font = '20px Verdana';
+            label.textAlign = 'right';
+            label.x = 140;
+            label.y = 200
+        });
+
+    clipSelection.append(Arc)
+        .each(arc => {
+            arc.centerX = 200;
+            arc.centerY = 100;
+            arc.radiusX = 100;
+            arc.radiusY = 100;
+            arc.startAngle = 0;
+            arc.endAngle = Math.PI * 2;
+            arc.fillStyle = 'lime';
+            arc.opacity = 0.7;
+        });
+}
