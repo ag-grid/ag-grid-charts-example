da2864b218a62bd5c7af5d7f9de775ea87d8676d
Vitaly Kravchenko <vitalyx@gmail.com>
2019-02-12 13:03:44 +0300

    More tests for the selection based Axis.

--- src/selection-axis-test/index.ts
+++ src/selection-axis-test/index.ts
@@ -10,6 +10,12 @@ function delay() {
     });
 }
 
+function nextFrame() {
+    return new Promise(resolve => {
+        requestAnimationFrame(resolve);
+    });
+}
+
 function renderAxes() {
     const padding = {
         top: 20,
@@ -40,14 +46,16 @@ function renderAxes() {
     // y-axis
     const yAxisGroup = new Group();
     const yAxis = new Axis<number>(yScale, yAxisGroup);
-    yAxis.translation = [padding.left, padding.top];
+    yAxis.translationX = padding.left;
+    yAxis.translationY = padding.top;
     yAxis.render();
 
     // x-axis
     const xAxisGroup = new Group();
     const xAxis = new Axis<string>(xScale, xAxisGroup);
     xAxis.rotation = -Math.PI / 2;
-    xAxis.translation = [padding.left, padding.top + seriesHeight];
+    xAxis.translationX = padding.left;
+    xAxis.translationY = padding.top + seriesHeight;
     xAxis.flippedLabels = true;
     xAxis.render();
 
@@ -78,6 +86,8 @@ function renderAxes() {
         yScale.range = [0, 300];
         yAxis.render();
     }).then(delay).then(() => {
+        console.assert(scene.frameIndex === 4);
+
         xScale.domain = ['Cat', 'Wolf', 'Sheep', 'Horse', 'Bear'];
         xScale.range = [0, 600];
         xAxis.rotation -= Math.PI / 5;
@@ -86,6 +96,31 @@ function renderAxes() {
         yScale.domain = [1.95, 2];
         yScale.range = [0, seriesHeight];
         yAxis.render();
+    }).then(delay).then(() => {
+        // In the previous block, the `render` method was called for both axes,
+        // but the scene graph is expected to render only once because all
+        // DOM changes are delayed until the next frame.
+        console.assert(scene.frameIndex === 5);
+
+        xAxis.render();
+        yAxis.render();
+    }).then(nextFrame).then(() => {
+        // In the previous block, we called the `render` method
+        // of both axes. This will result in the regeneration
+        // of the ticks (data), new data join on the existing
+        // selection of tick-label groups and update of the node
+        // attributes. But because the attributes calculated and set
+        // as the result of the described sequence are no different
+        // from the attributes calculated during the previous
+        // `render` call, no actual render (scene graph rasterisation)
+        // will happen. This feature is built into our scene graph's
+        // DOM itself - no rasterisation happens if the node attributes
+        // stay the same. A selection's job is to prevent the unnecessary
+        // generation or removal of DOM nodes. And no nodes are created
+        // or removed either because the regenerated tick data stayed
+        // the same, and so the `enter` and `exit` components of the
+        // new data join are of size zero.
+        console.assert(scene.frameIndex === 5);
     });
 }
 
