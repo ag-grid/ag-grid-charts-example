f9ff50bcb1b3a08faed60c39e6ca1428ef43c9e1
Vitaly Kravchenko <vitalyx@gmail.com>
2019-04-30 13:32:50 +0100

    `createSlider` method.

--- src/pie-series-example/index.ts
+++ src/pie-series-example/index.ts
@@ -38,6 +38,63 @@ function createButton(text: string, action: EventListenerOrEventListenerObject):
     return button;
 }
 
+function createSlider<D>(text: string, values: D[], action: (value: D) => void): HTMLInputElement {
+    const n = values.length;
+    const id = String(Date.now());
+    const sliderId = 'slider-' + id;
+    const datalistId = 'slider-list-' + id;
+    const wrapper = document.createElement('div');
+    wrapper.style.display = 'flex';
+    wrapper.style.alignItems = 'center';
+    wrapper.style.width = '250px';
+    wrapper.style.padding = '5px';
+    wrapper.style.margin = '5px';
+    wrapper.style.border = '1px solid lightgray';
+    wrapper.style.borderRadius = '5px';
+    wrapper.style.backgroundColor = 'white';
+
+    const slider = document.createElement('input');
+    slider.setAttribute('id', sliderId);
+    slider.setAttribute('list', datalistId);
+    slider.style.height = '1.8em';
+
+    const label = document.createElement('label');
+    label.setAttribute('for', sliderId);
+    label.innerHTML = text;
+    label.style.font = '12px sans-serif';
+    label.style.marginRight = '5px';
+
+    // Currently, no browser fully supports these features.
+    // https://developer.mozilla.org/en-US/docs/Web/HTML/Element/input/range
+    const datalist = document.createElement('datalist');
+    datalist.setAttribute('id', datalistId);
+
+    values.forEach((value, index) => {
+        const option = document.createElement('option');
+        option.setAttribute('value', String(index));
+        option.setAttribute('label', String(value));
+        datalist.appendChild(option);
+    });
+
+    slider.type = 'range';
+    slider.min = '0';
+    slider.max = String(n - 1);
+    slider.step = '1';
+    slider.value = '0';
+    slider.style.width = '200px';
+
+    wrapper.appendChild(label);
+    wrapper.appendChild(slider);
+    wrapper.appendChild(datalist);
+    document.body.appendChild(wrapper);
+
+    slider.addEventListener('input', (e) => {
+        const index = +(e.target as HTMLInputElement).value;
+        action(values[index]);
+    });
+    return slider;
+}
+
 function makeChartResizeable(chart: Chart<any, any, any>) {
     let startX = 0;
     let startY = 0;
