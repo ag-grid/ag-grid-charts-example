f362f270e791358f1c454a29a981ab7a6ca34741
Vitaly Kravchenko <vitalyx@gmail.com>
2019-06-08 19:27:45 +0100

    Add treemap example that uses ag-Grid's "tree" data.

--- src/treemap-test/index.ts
+++ src/treemap-test/index.ts
@@ -7,7 +7,7 @@ import { FinvizMapData } from "./finviz_data";
 import { HdpiCanvas } from "ag-grid-enterprise/src/charts/canvas/hdpiCanvas";
 import { Rect } from "ag-grid-enterprise/src/charts/scene/shape/rect";
 import { Text } from "ag-grid-enterprise/src/charts/scene/shape/text";
-import { createButton } from "../../lib/ui";
+import { convertGridTreeData, rowData } from "./convert";
 
 function processFinVizData() {
     console.log(FinvizMapData);
@@ -24,9 +24,34 @@ function processFinVizData() {
     console.log(JSON.stringify(process(FinvizMapData), null, 4));
 }
 
+function makeResizeable(scene: Scene, update: () => void) {
+    let startX = 0;
+    let startY = 0;
+    let isDragging = false;
+    let size: [number, number];
+
+    scene.hdpiCanvas.canvas.addEventListener('mousedown', (e: MouseEvent) => {
+        startX = e.offsetX;
+        startY = e.offsetY;
+        size = scene.size;
+        isDragging = true;
+    });
+    scene.hdpiCanvas.canvas.addEventListener('mousemove', (e: MouseEvent) => {
+        if (isDragging) {
+            const dx = e.offsetX - startX;
+            const dy = e.offsetY - startY;
+            scene.size = [size[0] + dx, size[1] + dy];
+            update();
+        }
+    });
+    scene.hdpiCanvas.canvas.addEventListener('mouseup', () => {
+        isDragging = false;
+    });
+}
+
 const fontName = '12px Verdana, sans-serif';
 
-document.addEventListener('DOMContentLoaded', () => {
+function createStockTreeMap() {
     const scene = new Scene(1200, 800);
     scene.parent = document.body;
 
@@ -102,36 +127,85 @@ document.addEventListener('DOMContentLoaded', () => {
     }
 
     update();
+    makeResizeable(scene, update);
+}
 
-    document.body.appendChild(document.createElement('br'));
-    createButton('Save', () => {
-        scene.download();
-    });
+function createOrgTreeMap() {
+    const data = convertGridTreeData(rowData);
 
-    function makeResizeable(scene: Scene) {
-        let startX = 0;
-        let startY = 0;
-        let isDragging = false;
-        let size: [number, number];
+    const scene = new Scene(1200, 800);
+    scene.parent = document.body;
 
-        scene.hdpiCanvas.canvas.addEventListener('mousedown', (e: MouseEvent) => {
-            startX = e.offsetX;
-            startY = e.offsetY;
-            size = scene.size;
-            isDragging = true;
+    const rootGroup = new Group();
+
+    scene.root = rootGroup;
+
+    let groupSelection: Selection<Group, Group, any, any> = Selection.select(rootGroup).selectAll<Group>();
+
+    const colorMap = new Map<Rect, string>();
+
+    function update() {
+        const [width, height] = scene.size;
+        const dataRoot = d3.hierarchy(data).sum(datum => datum.children ? 0 : 1);
+        const treemapLayout = d3.treemap().size([width, height]).round(true)
+            .paddingRight(4).paddingBottom(4).paddingLeft(4).paddingTop(node => {
+                const name = (node.data as any).orgHierarchy;
+                const nameSize = HdpiCanvas.getTextSize(name, fontName);
+                const width = node.x1 - node.x0;
+                const hasTitlePadding = node.depth > 0 && node.children && nameSize.width - 4 < width;
+                (node as any).hasTitle = hasTitlePadding;
+                return hasTitlePadding ? 15 : 4;
+            });
+        const descendants = treemapLayout(dataRoot).descendants();
+
+        // console.log(dataRoot);
+        // console.log(descendants);
+
+        const updateGroups = groupSelection.setData(descendants);
+        updateGroups.exit.remove();
+
+        const enterGroups = updateGroups.enter.append(Group);
+        enterGroups.append(Rect);
+        enterGroups.append(Text);
+
+        groupSelection = updateGroups.merge(enterGroups);
+
+        // @ts-ignore
+        const colorScale = d3.scaleLinear().domain([0, 2, 4]).range(['#d73027', '#fee08b', '#1a9850']);
+
+        groupSelection.selectByClass(Rect).each((rect, datum, index) => {
+            rect.fill = colorScale(datum.depth);
+            rect.stroke = 'black';
+            rect.strokeWidth = 1;
+            rect.crisp = true;
+
+            rect.x = datum.x0;
+            rect.y = datum.y0;
+            rect.width = datum.x1 - datum.x0;
+            rect.height = datum.y1 - datum.y0;
         });
-        scene.hdpiCanvas.canvas.addEventListener('mousemove', (e: MouseEvent) => {
-            if (isDragging) {
-                const dx = e.offsetX - startX;
-                const dy = e.offsetY - startY;
-                scene.size = [size[0] + dx, size[1] + dy];
-                update();
-            }
-        });
-        scene.hdpiCanvas.canvas.addEventListener('mouseup', () => {
-            isDragging = false;
+
+        groupSelection.selectByClass(Text).each((text, datum, index) => {
+            text.font = fontName;
+            text.textBaseline = 'top';
+
+            const isRoot = !datum.depth;
+            const isParent = !!datum.children;
+
+            text.text = datum.data.orgHierarchy;
+            const bbox = text.getBBox();
+            text.visible = !isRoot && (datum.x1 - datum.x0) - 2 * 2 > bbox.width && (datum.y1 - datum.y0) - 2 * 2 > bbox.height || datum.hasTitle;
+            text.x = datum.x0 + 3;
+            text.y = datum.y0 + 4;
         });
     }
 
-    makeResizeable(scene);
+    update();
+    makeResizeable(scene, update);
+}
+
+document.addEventListener('DOMContentLoaded', () => {
+    createStockTreeMap();
+    document.body.appendChild(document.createElement('br'));
+    createOrgTreeMap();
 });
