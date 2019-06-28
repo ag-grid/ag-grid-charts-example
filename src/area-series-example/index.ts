ca951ca95363d27faf666403b4450c9bc36aa3b8
Vitaly Kravchenko <vitalyx@gmail.com>
2019-06-28 15:15:05 +0100

    AG-2833 - Two new area chart examples: nuclear stockpiles with category and numeric axes.

--- src/area-series-example/index.ts
+++ src/area-series-example/index.ts
@@ -155,6 +155,170 @@ function makeChartResizeable(chart: Chart) {
     });
 }
 
+function makeNuclearChart() {
+    document.body.appendChild(document.createElement('br'));
+
+    const usaData = [
+        null, null, null, null, null, 6, 11, 32, 110, 235,
+        369, 640, 1005, 1436, 2063, 3057, 4618, 6444, 9822, 15468,
+        20434, 24126, 27387, 29459, 31056, 31982, 32040, 31233, 29224, 27342,
+        26662, 26956, 27912, 28999, 28965, 27826, 25579, 25722, 24826, 24605,
+        24304, 23464, 23708, 24099, 24357, 24237, 24401, 24344, 23586, 22380,
+        21004, 17287, 14747, 13076, 12555, 12144, 11009, 10950, 10871, 10824,
+        10577, 10527, 10475, 10421, 10358, 10295, 10104, 9914, 9620, 9326,
+        5113, 5113, 4954, 4804, 4761, 4717, 4368, 4018
+    ];
+    const ussrData = [null, null, null, null, null, null, null, null, null, null,
+        5, 25, 50, 120, 150, 200, 426, 660, 869, 1060,
+        1605, 2471, 3322, 4238, 5221, 6129, 7089, 8339, 9399, 10538,
+        11643, 13092, 14478, 15915, 17385, 19055, 21205, 23044, 25393, 27935,
+        30062, 32049, 33952, 35804, 37431, 39197, 45000, 43000, 41000, 39000,
+        37000, 35000, 33000, 31000, 29000, 27000, 25000, 24000, 23000, 22000,
+        21000, 20000, 19000, 18000, 18000, 17000, 16000, 15537, 14162, 12787,
+        12600, 11400, 5500, 4512, 4502, 4502, 4500, 4500
+    ];
+    const startX = 1940;
+
+    const data: {year: string, usa: number, ussr: number}[] = [];
+    const n = Math.min(usaData.length, ussrData.length);
+    for (let i = 0; i < n; i++) {
+        data.push({
+            year: String(startX + i),
+            usa: usaData[i]!,
+            ussr: ussrData[i]!
+        });
+    }
+
+    const xAxis = new CategoryAxis();
+    xAxis.scale.paddingInner = 1;
+    xAxis.scale.paddingOuter = 0;
+    xAxis.labelRotation = 45;
+    xAxis.labelFontSize = 10;
+    const yAxis = new NumberAxis();
+    const chart = new CartesianChart(xAxis, yAxis);
+    chart.parent = document.body;
+    chart.width = 1200;
+    chart.height = 400;
+    chart.title = Caption.create({
+        text: 'US and USSR nuclear stockpiles',
+        fontWeight: 'bold',
+        fontSize: 20,
+        fontFamily: 'Verdana, sans-serif'
+    });
+    chart.subtitle = Caption.create({
+        text: 'Sources: thebulletin.org & armscontrol.org',
+        fontSize: 12,
+        fontFamily: 'Verdana, sans-serif',
+        color: 'rgba(0, 0, 0, 0.6)'
+    });
+    chart.scene.hdpiCanvas.canvas.style.border = '1px solid black';
+
+    const usaArea = new AreaSeries();
+    usaArea.yFieldNames = ['USA'];
+    usaArea.xField = 'year';
+    usaArea.yFields = ['usa'];
+    usaArea.data = data;
+    usaArea.fills = ['rgba(255, 0, 0, 0.7)'];
+    usaArea.stroke = 'maroon';
+    usaArea.tooltipEnabled = true;
+
+    const ussrArea = new AreaSeries();
+    ussrArea.yFieldNames = ['USSR/Russia'];
+    ussrArea.xField = 'year';
+    ussrArea.yFields = ['ussr'];
+    ussrArea.data = data;
+    ussrArea.fills = ['rgba(0, 0, 255, 0.7)'];
+    ussrArea.stroke = 'darkblue';
+    ussrArea.tooltipEnabled = true;
+
+    chart.addSeries(usaArea);
+    chart.addSeries(ussrArea);
+
+    createSlider('skip labels', [1, 2, 3, 4, 5, 6, 7, 8, 9, 10], v => {
+        chart.xAxis.labelFormatter = params => {
+            return params.index % v === 0 ? params.value : '';
+        };
+        chart.performLayout();
+    });
+}
+
+function makeNuclearChartWithNumericX() {
+    document.body.appendChild(document.createElement('br'));
+
+    const usaData = [
+        null, null, null, null, null, 6, 11, 32, 110, 235,
+        369, 640, 1005, 1436, 2063, 3057, 4618, 6444, 9822, 15468,
+        20434, 24126, 27387, 29459, 31056, 31982, 32040, 31233, 29224, 27342,
+        26662, 26956, 27912, 28999, 28965, 27826, 25579, 25722, 24826, 24605,
+        24304, 23464, 23708, 24099, 24357, 24237, 24401, 24344, 23586, 22380,
+        21004, 17287, 14747, 13076, 12555, 12144, 11009, 10950, 10871, 10824,
+        10577, 10527, 10475, 10421, 10358, 10295, 10104, 9914, 9620, 9326,
+        5113, 5113, 4954, 4804, 4761, 4717, 4368, 4018
+    ];
+    const ussrData = [null, null, null, null, null, null, null, null, null, null,
+        5, 25, 50, 120, 150, 200, 426, 660, 869, 1060,
+        1605, 2471, 3322, 4238, 5221, 6129, 7089, 8339, 9399, 10538,
+        11643, 13092, 14478, 15915, 17385, 19055, 21205, 23044, 25393, 27935,
+        30062, 32049, 33952, 35804, 37431, 39197, 45000, 43000, 41000, 39000,
+        37000, 35000, 33000, 31000, 29000, 27000, 25000, 24000, 23000, 22000,
+        21000, 20000, 19000, 18000, 18000, 17000, 16000, 15537, 14162, 12787,
+        12600, 11400, 5500, 4512, 4502, 4502, 4500, 4500
+    ];
+    const startX = 1940;
+
+    const data: {year: number, usa: number, ussr: number}[] = [];
+    const n = Math.min(usaData.length, ussrData.length);
+    for (let i = 0; i < n; i++) {
+        data.push({
+            year: startX + i,
+            usa: usaData[i]!,
+            ussr: ussrData[i]!
+        });
+    }
+
+    const xAxis = new NumberAxis();
+    xAxis.nice = false;
+    const yAxis = new NumberAxis();
+    const chart = new CartesianChart(xAxis, yAxis);
+    chart.parent = document.body;
+    chart.width = 1200;
+    chart.height = 400;
+    chart.title = Caption.create({
+        text: 'US and USSR nuclear stockpiles',
+        fontWeight: 'bold',
+        fontSize: 20,
+        fontFamily: 'Verdana, sans-serif'
+    });
+    chart.subtitle = Caption.create({
+        text: 'Sources: thebulletin.org & armscontrol.org',
+        fontSize: 12,
+        fontFamily: 'Verdana, sans-serif',
+        color: 'rgba(0, 0, 0, 0.6)'
+    });
+    chart.scene.hdpiCanvas.canvas.style.border = '1px solid black';
+
+    const usaArea = new AreaSeries();
+    usaArea.yFieldNames = ['USA'];
+    usaArea.xField = 'year';
+    usaArea.yFields = ['usa'];
+    usaArea.data = data;
+    usaArea.fills = ['rgba(255, 0, 0, 0.7)'];
+    usaArea.stroke = 'maroon';
+    usaArea.tooltipEnabled = true;
+
+    const ussrArea = new AreaSeries();
+    ussrArea.yFieldNames = ['USSR/Russia'];
+    ussrArea.xField = 'year';
+    ussrArea.yFields = ['ussr'];
+    ussrArea.data = data;
+    ussrArea.fills = ['rgba(0, 0, 255, 0.7)'];
+    ussrArea.stroke = 'darkblue';
+    ussrArea.tooltipEnabled = true;
+
+    chart.addSeries(usaArea);
+    chart.addSeries(ussrArea);
+}
+
 document.addEventListener('DOMContentLoaded', () => {
     const xAxis = new CategoryAxis();
     xAxis.scale.paddingInner = 1;
@@ -408,4 +572,7 @@ document.addEventListener('DOMContentLoaded', () => {
     });
 
     makeChartResizeable(chart);
+
+    makeNuclearChart();
+    makeNuclearChartWithNumericX();
 });
