cc4eca7db124839113dd8788934a25ac459f7f6d
Vitaly Kravchenko <vitalyx@gmail.com>
2019-07-22 10:56:14 +0100

    Add the bar/column example.

---
+++ src/bar-column-example/index.ts
@@ -0,0 +1,280 @@
+import { ChartBuilder } from "ag-grid-enterprise/src/chartAdaptor/builder/chartBuilder";
+import { BarSeries } from "ag-grid-enterprise/src/charts/chart/series/barSeries";
+import { Chart } from "ag-grid-enterprise/src/charts/chart/chart";
+import borneo from "ag-grid-enterprise/src/charts/chart/palettes";
+
+import './app.css';
+import { createButton, createSlider } from "../../lib/ui";
+import { CartesianChart } from "ag-grid-enterprise/src/charts/chart/cartesianChart";
+import { CategoryAxis } from "ag-grid-enterprise/src/charts/chart/axis/categoryAxis";
+import { NumberAxis } from "ag-grid-enterprise/src/charts/chart/axis/numberAxis";
+import { Caption } from "ag-grid-enterprise/src/charts/caption";
+
+type Datum = {
+    category: string,
+
+    q1Budget: number,
+    q2Budget: number,
+    q3Budget: number,
+    q4Budget: number,
+
+    q1Actual: number,
+    q2Actual: number,
+    q3Actual: number,
+    q4Actual: number
+};
+
+const data: Datum[] = [
+    {
+        category: 'Coffee',
+
+        q1Budget: 500,
+        q2Budget: 500,
+        q3Budget: 500,
+        q4Budget: 500,
+
+        q1Actual: 450,
+        q2Actual: 560,
+        q3Actual: 600,
+        q4Actual: 700
+    },
+    {
+        category: 'Tea',
+
+        q1Budget: 350,
+        q2Budget: 400,
+        q3Budget: 450,
+        q4Budget: 500,
+
+        q1Actual: 270,
+        q2Actual: 380,
+        q3Actual: 450,
+        q4Actual: 520
+    },
+    {
+        category: 'Milk',
+
+        q1Budget: 200,
+        q2Budget: 180,
+        q3Budget: 180,
+        q4Budget: 180,
+
+        q1Actual: 180,
+        q2Actual: 170,
+        q3Actual: 190,
+        q4Actual: 200
+    },
+];
+
+type NegativeDatum = {
+    xField: string,
+    yField1: number,
+    yField2: number,
+    yField3: number
+};
+
+const negativeData: NegativeDatum[] = [{
+    xField: 'Jan',
+    yField1: 5,
+    yField2: 7,
+    yField3: -9,
+}, {
+    xField: 'Feb',
+    yField1: 10,
+    yField2: -15,
+    yField3: 20
+}];
+
+function makeChartResizeable(chart: Chart) {
+    let startX = 0;
+    let startY = 0;
+    let isDragging = false;
+    let chartSize: [number, number];
+    const scene = chart.scene;
+
+    scene.hdpiCanvas.canvas.addEventListener('mousedown', (e: MouseEvent) => {
+        startX = e.offsetX;
+        startY = e.offsetY;
+        chartSize = chart.size;
+        isDragging = true;
+    });
+    scene.hdpiCanvas.canvas.addEventListener('mousemove', (e: MouseEvent) => {
+        if (isDragging) {
+            const dx = e.offsetX - startX;
+            const dy = e.offsetY - startY;
+            chart.size = [chartSize[0] + dx, chartSize[1] + dy];
+        }
+    });
+    scene.hdpiCanvas.canvas.addEventListener('mouseup', () => {
+        isDragging = false;
+    });
+}
+
+function createColumnChart() {
+    const chart = ChartBuilder.createCartesianChart({
+        parent: document.body,
+        width: 800,
+        height: 500,
+        title: {
+            text: 'Beverage Expenses'
+        },
+        subtitle: {
+            text: 'per quarter'
+        },
+        xAxis: {
+            type: 'category',
+            labelRotation: 0,
+            title: {
+                text: 'Beverage'
+            }
+        },
+        yAxis: {
+            type: 'number',
+            title: {
+                text: 'Expenses'
+            }
+        }
+    });
+    // chart.xAxis.title = Caption.create({
+    //     text: 'Beverage'
+    // });
+    // chart.yAxis.title = Caption.create({
+    //     text: 'Expenses'
+    // });
+    chart.scene.hdpiCanvas.canvas.style.border = '1px solid black';
+
+    function addSeriesIf() {
+        if (!chart.series.length) {
+            chart.addSeries(barSeries);
+        }
+    }
+
+    const barSeries = new BarSeries();
+    addSeriesIf();
+    barSeries.yFieldNames = ['Q1', 'Q2', 'Q3', 'Q4']; // bar labels
+    barSeries.xField = 'category';
+    barSeries.yFields = ['q1Actual', 'q2Actual', 'q3Actual', 'q4Actual'];
+    barSeries.data = data;
+    barSeries.fills = borneo.fills;
+    barSeries.tooltipEnabled = true;
+    barSeries.labelEnabled = false;
+
+    document.body.appendChild(document.createElement('br'));
+
+    createButton('Save Chart Image', () => {
+        chart.scene.download('bar-chart');
+    });
+
+    createButton('Negative Data', () => {
+        barSeries.data = negativeData;
+        barSeries.xField = 'xField';
+        barSeries.yFields = ['yField1', 'yField2', 'yField3'];
+    });
+
+    createButton('Grouped', () => {
+        barSeries.grouped = true;
+    });
+    createButton('Stacked', () => {
+        barSeries.grouped = false;
+    });
+
+    createButton('Enable labels', () => {
+        barSeries.labelEnabled = true;
+    });
+    createButton('Disable labels', () => {
+        barSeries.labelEnabled = false;
+    });
+
+    createSlider('normalizeTo', [NaN, 100, 500, 1], v => {
+        if (v && chart.title) {
+            chart.title.text = 'Normalize to WTFYW';
+            chart.subtitle = undefined;
+        }
+        barSeries.normalizedTo = v;
+    });
+
+    return chart;
+}
+
+function createBarChart() {
+    const chart = new CartesianChart(new NumberAxis(), new CategoryAxis());
+
+    chart.parent = document.body;
+    chart.width = 800;
+    chart.height = 500;
+    chart.layout = 'horizontal';
+    chart.scene.hdpiCanvas.canvas.style.border = '1px solid black';
+
+    function addSeriesIf() {
+        if (!chart.series.length) {
+            chart.addSeries(barSeries);
+        }
+    }
+
+    const barSeries = new BarSeries();
+    addSeriesIf();
+    barSeries.yFieldNames = ['Q1', 'Q2', 'Q3', 'Q4']; // bar labels
+    barSeries.xField = 'category';
+    barSeries.yFields = ['q1Actual', 'q2Actual', 'q3Actual', 'q4Actual'];
+    barSeries.data = data;
+    barSeries.fills = borneo.fills;
+    barSeries.tooltipEnabled = true;
+    barSeries.labelEnabled = false;
+
+    document.body.appendChild(document.createElement('br'));
+
+    createButton('Save Chart Image', () => {
+        chart.scene.download('bar-chart');
+    });
+
+    createButton('Negative Data', () => {
+        barSeries.data = negativeData;
+        barSeries.xField = 'xField';
+        barSeries.yFields = ['yField1', 'yField2', 'yField3'];
+    });
+
+    createButton('Grouped', () => {
+        barSeries.grouped = true;
+    });
+    createButton('Stacked', () => {
+        barSeries.grouped = false;
+    });
+
+    createButton('Enable labels', () => {
+        barSeries.labelEnabled = true;
+    });
+    createButton('Disable labels', () => {
+        barSeries.labelEnabled = false;
+    });
+
+    createSlider('normalizeTo', [NaN, 100, 500, 1], v => {
+        if (v && chart.title) {
+            chart.title.text = 'Normalize to WTFYW';
+            chart.subtitle = undefined;
+        }
+        barSeries.normalizedTo = v;
+    });
+
+    createSlider('stroke width', [1, 2, 4, 6, 8, 10], v => {
+        barSeries.strokeWidth = v;
+    });
+
+    createSlider('stroke opacity', [1, 0.9, 0.8, 0.7, 0.6, 0.5, 0.4, 0.3, 0.2, 0.1, 0], v => {
+        barSeries.strokeOpacity = v;
+    });
+    createSlider('fill opacity', [1, 0.9, 0.8, 0.7, 0.6, 0.5, 0.4, 0.3, 0.2, 0.1, 0], v => {
+        barSeries.fillOpacity = v;
+    });
+
+    return chart;
+}
+
+document.addEventListener('DOMContentLoaded', () => {
+    const columnChart = createColumnChart();
+    makeChartResizeable(columnChart);
+
+    document.body.appendChild(document.createElement('br'));
+
+    const barChart = createBarChart();
+    makeChartResizeable(barChart);
+});
