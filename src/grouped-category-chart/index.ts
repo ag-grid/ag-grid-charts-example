d651c012286bb5099d3399aaa5191d8ef1e9d09e
Vitaly Kravchenko <vitalyx@gmail.com>
2019-08-01 11:52:58 +0100

    AG-3159: Update the grouped category axis example to use tree data.

--- src/grouped-category-chart/index.ts
+++ src/grouped-category-chart/index.ts
@@ -1,42 +1,70 @@
-import { CartesianChart } from "ag-grid-enterprise/src/charts/chart/cartesianChart";
-import { CategoryAxis } from "ag-grid-enterprise/src/charts/chart/axis/categoryAxis";
-import { NumberAxis } from "ag-grid-enterprise/src/charts/chart/axis/numberAxis";
-import { LineSeries } from "ag-grid-enterprise/src/charts/chart/series/lineSeries";
-import { createButton } from "../../lib/ui";
-import { GroupedCategoryAxis } from "ag-grid-enterprise/src/charts/chart/axis/groupedCategoryAxis";
-import { GroupedCategoryChart } from "ag-grid-enterprise/src/charts/chart/groupedCategoryChart";
-import { BarSeries } from "ag-grid-enterprise/src/charts/chart/series/barSeries";
-import { Chart } from "ag-grid-enterprise/src/charts/chart/chart";
+import { CartesianChart } from 'ag-grid-enterprise/src/charts/chart/cartesianChart';
+import { CategoryAxis } from 'ag-grid-enterprise/src/charts/chart/axis/categoryAxis';
+import { NumberAxis } from 'ag-grid-enterprise/src/charts/chart/axis/numberAxis';
+import { LineSeries } from 'ag-grid-enterprise/src/charts/chart/series/lineSeries';
+import { createButton } from '../../lib/ui';
+import { GroupedCategoryAxis } from 'ag-grid-enterprise/src/charts/chart/axis/groupedCategoryAxis';
+import { GroupedCategoryChart } from 'ag-grid-enterprise/src/charts/chart/groupedCategoryChart';
+import { BarSeries } from 'ag-grid-enterprise/src/charts/chart/series/barSeries';
+import { Chart } from 'ag-grid-enterprise/src/charts/chart/chart';
 
 type CategoryDatum = {
-    category: string,
+    category: {labels: string[]},
     value: number,
     value2: number
 };
 
+// type CategoryDatum = {
+//     category: string,
+//     value: number,
+//     value2: number
+// };
+//
+// const categoryData: CategoryDatum[] = [
+//     {category: 'John', value: 3, value2: 2},
+//     {category: 'Nige', value: 7, value2: 5},
+//     {category: 'Vicky', value: 6, value2: 3},
+//     {category: 'Rick', value: 4, value2: 7},
+//     {category: 'Lucy', value: 8, value2: 5},
+//     {category: 'Ben', value: 5, value2: 2},
+//     {category: 'Barbara', value: 6, value2: 3},
+//     {category: 'Maria', value: 3, value2: 4}
+// ];
+
+// const categoryData: CategoryDatum[] = [
+//     {category: {labels: ['A', 'B', 'F', 'O']}, value: 3, value2: 2},
+//     {category: {labels: ['C', 'E', 'F', 'O']}, value: 7, value2: 5},
+//     {category: {labels: ['D', 'E', 'F', 'O']}, value: 6, value2: 3},
+//     {category: {labels: ['G', 'I', 'N', 'O']}, value: 4, value2: 7},
+//     {category: {labels: ['H', 'I', 'N', 'O']}, value: 8, value2: 5},
+//     {category: {labels: ['J', 'M', 'N', 'O']}, value: 5, value2: 2},
+//     {category: {labels: ['K', 'M', 'N', 'O']}, value: 6, value2: 3},
+//     {category: {labels: ['L', 'M', 'N', 'O']}, value: 3, value2: 4}
+// ];
+
 const categoryData: CategoryDatum[] = [
-    {category: 'John', value: 3, value2: 2},
-    {category: 'Nige', value: 7, value2: 5},
-    {category: 'Vicky', value: 6, value2: 3},
-    {category: 'Rick', value: 4, value2: 7},
-    {category: 'Lucy', value: 8, value2: 5},
-    {category: 'Ben', value: 5, value2: 2},
-    {category: 'Barbara', value: 6, value2: 3},
-    {category: 'Maria', value: 3, value2: 4}
+    {category: {labels: ['London', 'England', 'UK', 'Europe']}, value: 3, value2: 2},
+    {category: {labels: ['Bristol', 'England', 'UK', 'Europe']}, value: 7, value2: 5},
+    {category: {labels: ['Glasgow', 'Scotland', 'UK', 'Europe']}, value: 6, value2: 3},
+    {category: {labels: ['Austin', 'Texas', 'USA', 'North America']}, value: 4, value2: 7},
+    {category: {labels: ['Seattle', 'Washington', 'USA', 'North America']}, value: 8, value2: 5},
+    {category: {labels: ['San Jose', 'California', 'USA', 'North America']}, value: 5, value2: 2},
+    {category: {labels: ['San Diego', 'California', 'USA', 'North America']}, value: 6, value2: 3},
+    {category: {labels: ['San Francisco', 'California', 'USA', 'North America']}, value: 3, value2: 4}
 ];
 
-function generateCategoryData(n = 50): CategoryDatum[] {
-    const data: CategoryDatum[] = [];
-    for (let i = 0; i < n; i++) {
-        const datum: CategoryDatum = {
-            category: 'A' + (i + 1),
-            value: Math.random() * 10,
-            value2: Math.random() * 10
-        };
-        data.push(datum);
-    }
-    return data;
-}
+// function generateCategoryData(n = 50): CategoryDatum[] {
+//     const data: CategoryDatum[] = [];
+//     for (let i = 0; i < n; i++) {
+//         const datum: CategoryDatum = {
+//             category: 'A' + (i + 1),
+//             value: Math.random() * 10,
+//             value2: Math.random() * 10
+//         };
+//         data.push(datum);
+//     }
+//     return data;
+// }
 
 function makeChartResizeable(chart: Chart) {
     let startX = 0;
@@ -81,6 +109,7 @@ function createCategoryChart() {
     barSeries.xField = 'category';
     barSeries.yFields = ['value', 'value2'];
     barSeries.grouped = true;
+    barSeries.fillOpacity = 0.3;
 
     const lineSeries = new LineSeries();
     lineSeries.marker = true;
@@ -97,11 +126,11 @@ function createCategoryChart() {
         chart.scene.download('chart');
     });
 
-    createButton('Change data', () => {
-        lineSeries.data = generateCategoryData(Math.floor(Math.random() * 50));
-        lineSeries.xField = 'category';
-        lineSeries.yField = 'value';
-    });
+    // createButton('Change data', () => {
+    //     lineSeries.data = generateCategoryData(Math.floor(Math.random() * 50));
+    //     lineSeries.xField = 'category';
+    //     lineSeries.yField = 'value';
+    // });
 
     createButton('No data', () => {
         lineSeries.data = [];
