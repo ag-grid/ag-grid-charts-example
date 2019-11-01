4d53effc53c41f5148a286a0fc8cb4caaf4281cd
Vitaly Kravchenko <vitalyx@gmail.com>
2019-11-01 13:01:48 +0000

    AG-3390 - Create makeChartResizeable helper method.

---
+++ lib/chart.ts
@@ -0,0 +1,26 @@
+import { Chart } from "@ag-enterprise/grid-charts/src/charts/chart/chart";
+
+export function makeChartResizeable(chart: Chart) {
+    let startX = 0;
+    let startY = 0;
+    let isDragging = false;
+    let chartSize: [number, number];
+    const scene = chart.scene;
+
+    scene.canvas.element.addEventListener('mousedown', (e: MouseEvent) => {
+        startX = e.offsetX;
+        startY = e.offsetY;
+        chartSize = chart.size;
+        isDragging = true;
+    });
+    scene.canvas.element.addEventListener('mousemove', (e: MouseEvent) => {
+        if (isDragging) {
+            const dx = e.offsetX - startX;
+            const dy = e.offsetY - startY;
+            chart.size = [chartSize[0] + dx, chartSize[1] + dy];
+        }
+    });
+    scene.canvas.element.addEventListener('mouseup', () => {
+        isDragging = false;
+    });
+}
