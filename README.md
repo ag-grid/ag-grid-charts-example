e6627ef31bf280cbf7e3fe8db6d6b87ec9fadac0
Adrian Lowdon <adrian@acceleratech.uk>
2019-11-26 11:37:00 +0000

    AG-3390 - added option to build specific example

--- README.md
+++ README.md
@@ -6,6 +6,8 @@ Run `npm run bundle` to perform full TypeScript compilation and bundle all examp
 
 Run `npm run bundleQuick` to only transpile and bundle - no type checking will be performed.
 
-If you are actively developing a particular example, rename the example folder to `_` (one underscore). Now when you run the builds,
-only this example will be built, type-checking will be performed, and watch mode will automatically be enabled. Make sure not to
-check in this state!
+If you are actively developing a particular example, you can specify the example to build by calling
+
+`npm run bundleQuick -- --env.example=your-example-name`
+
+This will build only the specified example, but type-checking will be performed, and watch mode will automatically be enabled. Alternatively, you can rename an example folder to `_` (one underscore) to achieve the same, but make sure not to check in this state!
--- webpack.config.js
+++ webpack.config.js
@@ -7,6 +7,7 @@ const isDirectory = path => lstatSync(path).isDirectory();
 const getExamples = path => {
     const files = readdirSync(path);
     const examples = [];
+
     files.forEach(name => {
         if (name === 'lib') {
             throw new Error("Example folder can't be named 'lib' " +
@@ -22,73 +23,77 @@ const getExamples = path => {
             examples.push({ path, name });
         }
     });
+
     return examples;
 };
 
-// Note: empty directories in the source folder will result in a bundling error.
-// All directories in source folder should contain a file that serves as an entry,
-// as specified below.
-const srcPath = path.resolve(__dirname, 'src');
-const distPath = path.resolve(__dirname, 'dist');
-// If we have the `_` directory in the `srcPath`, process only that.
-// No need to re-bundle all the examples when one typically works
-// on a single example at a time. This example is supposed to be
-// in the `_` directory that should be given a proper name when
-// one is done working on the example.
-const activeExampleName = '_';
-const hasActiveExample = existsSync(join(srcPath, activeExampleName));
-const examples = hasActiveExample ? [{ path: srcPath, name: activeExampleName }] : getExamples(srcPath);
+const moduleExports = env => {
+    // Note: empty directories in the source folder will result in a bundling error.
+    // All directories in source folder should contain a file that serves as an entry,
+    // as specified below.
+    const srcPath = path.resolve(__dirname, 'src');
+    const distPath = path.resolve(__dirname, 'dist');
 
-const moduleExports = examples.map(example => {
-    return {
-        entry: join(example.path, example.name, 'index'),
-        mode: 'development',
-        target: 'web',
-        devtool: 'inline-source-map',
-        output: {
-            path: join(distPath, example.name),
-            filename: 'app.bundle.js'
-        },
-        stats: 'errors-only',
-        watch: hasActiveExample,
-        resolve: {
-            extensions: ['.ts', '.tsx', '.js', '.json']
-        },
-        module: {
-            rules: [{
-                test: /\.tsx?$/,
-                use: [{
-                    loader: 'ts-loader',
-                    options: {
-                        onlyCompileBundledFiles: true,
-                        transpileOnly: !hasActiveExample
-                    }
-                }],
-                exclude: /node_modules/
-            }, {
-                test: /\.css$/,
-                use: ['style-loader', 'css-loader'],
-                exclude: /node_modules/
-            }, {
-                test: /\.(png|jpg|gif)$/i,
-                use: [
-                    {
-                        loader: 'url-loader',
+    // If we have the `_` directory in the `srcPath`, process only that.
+    // No need to re-bundle all the examples when one typically works
+    // on a single example at a time. This example is supposed to be
+    // in the `_` directory that should be given a proper name when
+    // one is done working on the example.
+    const activeExampleName = env && env.example ? env.example : '_';
+    const hasActiveExample = existsSync(join(srcPath, activeExampleName));
+    const examples = hasActiveExample ? [{ path: srcPath, name: activeExampleName }] : getExamples(srcPath);
+
+    return examples.map(example => {
+        return {
+            entry: join(example.path, example.name, 'index'),
+            mode: 'development',
+            target: 'web',
+            devtool: 'inline-source-map',
+            output: {
+                path: join(distPath, example.name),
+                filename: 'app.bundle.js'
+            },
+            stats: 'errors-only',
+            watch: hasActiveExample,
+            resolve: {
+                extensions: ['.ts', '.tsx', '.js', '.json']
+            },
+            module: {
+                rules: [{
+                    test: /\.tsx?$/,
+                    use: [{
+                        loader: 'ts-loader',
                         options: {
-                            limit: 8192,
+                            onlyCompileBundledFiles: true,
+                            transpileOnly: !hasActiveExample
+                        }
+                    }],
+                    exclude: /node_modules/
+                }, {
+                    test: /\.css$/,
+                    use: ['style-loader', 'css-loader'],
+                    exclude: /node_modules/
+                }, {
+                    test: /\.(png|jpg|gif)$/i,
+                    use: [
+                        {
+                            loader: 'url-loader',
+                            options: {
+                                limit: 8192,
+                            },
                         },
-                    },
-                ],
-                exclude: /node_modules/
-            }]
-        },
-        plugins: [
-            new HtmlWebpackPlugin({
-                filename: 'index.html'
-            }),
-        ],
-    };
-});
+                    ],
+                    exclude: /node_modules/
+                }]
+            },
+            plugins: [
+                new HtmlWebpackPlugin({
+                    filename: 'index.html'
+                }),
+            ],
+        };
+    });
+};
 
 // console.log(moduleExports);
 
