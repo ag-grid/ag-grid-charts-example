1974d4b56ce79c671d6ee4ba302281f3d7afe722
Vitaly Kravchenko <vitalyx@gmail.com>
2019-12-05 11:27:13 +0000

    Add text node performance test.

--- src/scene-test-arc-path/index.ts
+++ src/scene-test-arc-path/index.ts
@@ -2,8 +2,9 @@ import {Scene} from "@ag-grid-enterprise/charts/src/charts/scene/scene";
 import {Group} from "@ag-grid-enterprise/charts/src/charts/scene/group";
 import {Arc, ArcType} from "@ag-grid-enterprise/charts/src/charts/scene/shape/arc";
 import {FpsCounter} from "@ag-grid-enterprise/charts/src/charts/scene/fpsCounter";
+import { FontWeight, Text } from "@ag-grid-enterprise/charts/src/charts/scene/shape/text";
 
-document.addEventListener('DOMContentLoaded', () => {
+function createArcPathExample() {
     const scene = new Scene();
     scene.width = 800;
     scene.height = 400;
@@ -64,4 +65,70 @@ document.addEventListener('DOMContentLoaded', () => {
 
         requestAnimationFrame(step);
     })();
+}
+
+function createTextExample() {
+    const scene = new Scene();
+    scene.width = 1200;
+    scene.height = 800;
+    scene.parent = document.body;
+
+    const group = new Group();
+
+    const n = 1000;
+    const width = scene.width;
+    const height = scene.height;
+
+    const nodes: Text[] = [];
+    const deltas: [number, number][] = [];
+    for (let i = 0; i < n; i++) {
+        const node = new Text();
+        node.x = Math.random() * width;
+        node.y = Math.random() * height;
+        node.text = 'ag-Grid';
+        node.fill = 'red';
+        node.fontSize = 20;
+        node.fontWeight = 'bold';
+        node.stroke = 'black';
+        node.strokeWidth = 2;
+        nodes.push(node);
+
+        deltas.push([Math.random() - 0.5, Math.random() - 0.5]);
+    }
+    group.append(nodes);
+
+    scene.root = group;
+
+    const fpsCounter = new FpsCounter(document.body);
+
+    (function step() {
+        fpsCounter.countFrame();
+        nodes.forEach((node, i) => {
+            const delta = deltas[i];
+
+            node.x += delta[0];
+            node.y += delta[1];
+
+            if (node.x > width) {
+                node.x -= width;
+            }
+            else if (node.x < 0) {
+                node.x += width;
+            }
+
+            if (node.y > height) {
+                node.y -= height;
+            }
+            else if (node.y < 0) {
+                node.y += height;
+            }
+        });
+
+        requestAnimationFrame(step);
+    })();
+}
+
+document.addEventListener('DOMContentLoaded', () => {
+    createArcPathExample();
+    // createTextExample();
 });
