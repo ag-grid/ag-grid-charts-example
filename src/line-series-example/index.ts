618e60d03c0ca5480045acfddd57ef604f97ee2d
Vitaly Kravchenko <vitalyx@gmail.com>
2020-01-24 13:54:48 +0000

    Cleanup line series example. Make marker stroke width slider work correctly again.

--- src/line-series-example/index.ts
+++ src/line-series-example/index.ts
@@ -9,6 +9,8 @@ import { ChartAxisPosition } from "ag-charts-community/src/chart/chartAxis";
 import { Square } from "ag-charts-community/src/chart/marker/square";
 import { makeChartResizeable } from "../../lib/chart";
 import { Chart } from "ag-charts-community";
+import { AgChart } from "ag-charts-community/src/chart/agChart";
+import { createButton, createSlider } from "../../lib/ui";
 
 type CategoryDatum = {
     category: string,
@@ -122,73 +124,6 @@ function generateSpiralData(): NumericDatum[] {
     }
     return data;
 }
-
-function createButton(text: string, action: EventListenerOrEventListenerObject): HTMLButtonElement {
-    const button = document.createElement('button');
-    button.textContent = text;
-    document.body.appendChild(button);
-    button.addEventListener('click', action);
-    return button;
-}
-
-function createSlider<D>(text: string, values: D[], action: (value: D) => void): HTMLInputElement {
-    const n = values.length;
-    const id = String(Date.now());
-    const sliderId = 'slider-' + id;
-    const datalistId = 'slider-list-' + id;
-    const wrapper = document.createElement('div');
-    wrapper.style.display = 'inline-flex';
-    wrapper.style.alignItems = 'center';
-    wrapper.style.width = '300px';
-    wrapper.style.padding = '5px';
-    wrapper.style.margin = '5px';
-    wrapper.style.border = '1px solid lightgray';
-    wrapper.style.borderRadius = '5px';
-    wrapper.style.backgroundColor = 'white';
-
-    const slider = document.createElement('input');
-    slider.setAttribute('id', sliderId);
-    slider.setAttribute('list', datalistId);
-    slider.style.height = '1.8em';
-    slider.style.flex = '1';
-
-    const label = document.createElement('label');
-    label.setAttribute('for', sliderId);
-    label.innerHTML = text;
-    label.style.font = '12px sans-serif';
-    label.style.marginRight = '5px';
-
-    // Currently, no browser fully supports these features.
-    // https://developer.mozilla.org/en-US/docs/Web/HTML/Element/input/range
-    const datalist = document.createElement('datalist');
-    datalist.setAttribute('id', datalistId);
-
-    values.forEach((value, index) => {
-        const option = document.createElement('option');
-        option.setAttribute('value', String(index));
-        option.setAttribute('label', String(value));
-        datalist.appendChild(option);
-    });
-
-    slider.type = 'range';
-    slider.min = '0';
-    slider.max = String(n - 1);
-    slider.step = '1';
-    slider.value = '0';
-    slider.style.width = '200px';
-
-    wrapper.appendChild(label);
-    wrapper.appendChild(slider);
-    wrapper.appendChild(datalist);
-    document.body.appendChild(wrapper);
-
-    slider.addEventListener('input', (e) => {
-        const index = +(e.target as HTMLInputElement).value;
-        action(values[index]);
-    });
-    return slider;
-}
-
 
 function createCategoryLineChart() {
     const xAxis = new CategoryAxis();
@@ -457,9 +392,9 @@ function createNumericLineChart() {
         chart.layoutPending = true;
     });
 
-    createSlider('lineWidth', [0, 2, 4, 6, 8], value => lineSeries.strokeWidth = value);
-    createSlider('markerLineWidth', [0, 2, 4, 6, 8], value => lineSeries.strokeWidth = value);
-    createSlider('markerSize', [0, 2, 4, 6, 8], value => lineSeries.marker.size = value);
+    createSlider('stroke width', [0, 2, 4, 6, 8], value => lineSeries.strokeWidth = value);
+    createSlider('marker stroke width', [0, 2, 4, 6, 8], value => lineSeries.marker.strokeWidth = value);
+    createSlider('marker size', [0, 2, 4, 6, 8], value => lineSeries.marker.size = value);
 }
 
 function createMultiLineChart() {
@@ -591,9 +526,53 @@ function createMultiLineChart() {
     });
 }
 
+function createBasicLineChartUsingFactory() {
+    const coffeeSpending = [
+        {
+            quarter: 'Q1',
+            spending: 450
+        },
+        {
+            quarter: 'Q2',
+            spending: 560
+        },
+        {
+            quarter: 'Q3',
+            spending: 600
+        },
+        {
+            quarter: 'Q4',
+            spending: 700
+        }
+    ];
+
+    // const chart = AgChart.create({
+    //     data: coffeeSpending,
+    //     container: document.body,
+    //     series: [{
+    //         xKey: 'quarter',
+    //         yKey: 'spending'
+    //     }]
+    // });
+
+    const chart = AgChart.create({
+        data: coffeeSpending,
+        container: document.body,
+        series: [{
+            xKey: 'quarter',
+            yKey: 'spending',
+            yName: 'Coffee Spending'
+        }],
+        legend: {
+            position: 'bottom'
+        }
+    });
+}
+
 document.addEventListener('DOMContentLoaded', () => {
     createTwoVerticalAxesLineChart();
     createCategoryLineChart();
     createNumericLineChart();
     createMultiLineChart();
+    createBasicLineChartUsingFactory();
 });
