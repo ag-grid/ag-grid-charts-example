b81e4f25e30246d544dcf8bac1a2a419d42f6c5a
removedporn <86824510+removedporn@users.noreply.github.com>
2022-05-17 23:33:49 +0800

    Delete src directory

--- src/mana/padding.ts
+++
@@ -1,8 +0,0 @@
-import { Observable, reactive } from "../../charts/util/observable"
-
-export class Padding extends Observable {
-    @reactive('paddingChange') top: number = 50;
-    @reactive('paddingChange') right: number = 50;
-    @reactive('paddingChange') bottom: number = 50;
-    @reactive('paddingChange') left: number = 50;
-}
