34689ea3319cb72eadea145ca4f7ff821a1e194e
removedporn <86824510+removedporn@users.noreply.github.com>
2022-05-18 08:34:56 +0800

    Delete setup.mjs

--- setup.mjs
+++
@@ -1,5 +0,0 @@
-import fs from 'fs';
-import path from 'path';
-
-fs.symlinkSync(path.resolve('.', '../ag-grid/charts-packages/ag-charts-community/src'), 'charts', 'dir');
-fs.symlinkSync(path.resolve('.', '../ag-grid/enterprise-modules/sparklines/src'), 'sparklines', 'dir');
